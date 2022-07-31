5d7711fe2251405aa71c5a44d2e91dd54e5a32d1
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-07-31 15:18:05 -0300

    add new items

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,23 @@
+class ContactController {
+  index() {
+    // Listar todos registros
+  }
+
+  show() {
+    // Obter um registro
+  }
+
+  store() {
+    // Criar um novo registro
+  }
+
+  update() {
+    // Atualizar um registro
+  }
+
+  delete() {
+    // Deletar um registro
+  }
+}
+
+module.exports = new ContactController();
