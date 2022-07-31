4c7cab83407ce0dc466f675dc904af632ab1ae42
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-07-31 18:33:38 -0300

    adiciona conexao com banco de dados e tratamento de error

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -1,22 +1,97 @@
+const ContactsRepository = require('../repositories/ContactsRepository');
+
 class ContactController {
-  index() {
+  async index(request, response) {
     // Listar todos registros
+    const { orderBy } = request.query;
+    const contacts = await ContactsRepository.findAll(orderBy);
+
+    response.json(contacts);
   }
 
-  show() {
+  async show(request, response) {
     // Obter um registro
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({
+        error: 'User not found',
+      });
+    }
+
+    response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // Criar um novo registro
+
+    const {
+      name,
+      email,
+      phone,
+      category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactsExists = await ContactsRepository.findByEmail(email);
+
+    if (contactsExists) {
+      return response.status(400).json({ error: 'This email already in use' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    response.json(contact);
   }
 
-  update() {
+  async update(request, response) {
     // Atualizar um registro
+    const { id } = request.params;
+    const {
+      name,
+      email,
+      phone,
+      category_id,
+    } = request.body;
+
+    const contactsExists = await ContactsRepository.findById(id);
+
+    if (!contactsExists) {
+      return response.status(400).json({ error: 'User not found' });
+    }
+
+    const contactByEmailExists = await ContactsRepository.findByEmail(email);
+
+    if (contactByEmailExists && contactByEmailExists.id !== id) {
+      return response.status(400).json({ error: 'This email already in use' });
+    }
+
+    const contact = await ContactsRepository.update(id, {
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    response.json(contact);
   }
 
-  delete() {
+  async delete(request, response) {
     // Deletar um registro
+    const { id } = request.params;
+
+    await ContactsRepository.delete(id);
+
+    response.sendStatus(204);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const routes = require('./routes');
 
 const app = express();
 
+// Posibilita que os bodys dos requests cheguem no formato JSON
+app.use(express.json());
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  console.error('Error Handler');
+  console.error(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server is running on port 3000!'));
